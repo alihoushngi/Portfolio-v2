34069fcb3db0060ed6910916fe416cb4ad6ded78
alihoushngi <alihoushngi@gmail.com>
2023-08-26 18:08:51 +0330

    resume added

--- src/components/Base/Navbar/Navbar.tsx
+++ src/components/Base/Navbar/Navbar.tsx
@@ -5,8 +5,15 @@ import Berger from "./Burger/Burger";
 
 const Navbar = () => {
   return (
-    <header className="max-md:py-5 max-sm:container px-5 flex justify-center max-l:justify-end md:p-6 items-center">
+    <header className="max-md:py-5 max-sm:container px-5 flex justify-center max-l:justify-end md:p-6 items-center gap-5">
       <Berger />
+      <a
+        className="p-2 bg-slate-400 rounded-lg text-black text-sm font-light hover:bg-slate-800 hover:text-white hover:font-medium transition-all"
+        href="https://drive.google.com/file/d/1N3mtqYjM2cAfVp2aLOXW6zaZhYmFcbfq/view?usp=sharing"
+        target="_blank"
+      >
+        Resume
+      </a>
     </header>
   );
 };
