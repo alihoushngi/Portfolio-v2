959491f69defe9ba120d5e1fbbeb06077ba62715
alihoushngi <alihoushngi@gmail.com>
2023-09-23 19:08:46 +0330

    blog page cwith typing style created

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,22 @@
+import Layout from "@/components/Base/Layout/Layout";
+import Navbar from "@/components/Base/Navbar/Navbar";
+import TypingStyle from "@/components/Shared/TypingStyle/TypingStyle";
 import React from "react";
 
 const page = () => {
-  return <div>blog</div>;
+  return (
+    <>
+      <Navbar />
+      <Layout customClass="flex-row max-sm:flex-col-reverse">
+        <div className="flex justify-center items-center text-center w-fit  text-xl select-none ">
+          <TypingStyle>
+            I am working hard to bring you valuable content. Please revisit this
+            page shortly.
+          </TypingStyle>
+        </div>
+      </Layout>
+    </>
+  );
 };
 
 export default page;
